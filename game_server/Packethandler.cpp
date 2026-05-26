4309285bec46bd95128422fb1a15ac86911a489b
sewqp <mochayo77@gmail.com>
2026-05-26 15:25:37 +0900

    fix: Packet.h 인코딩 수정 및 PacketHandler char 배열 안전 처리
    - Packet.h CP949 → UTF-8 재작성 (주석 한글 깨짐 수정)
    - Packethandler.cpp std::string(char*) → strnlen 버전으로 교체
      (네트워크 패킷 null 미보장 상황 대비)

--- game_server/Packethandler.cpp
+++ game_server/Packethandler.cpp
@@ -1,6 +1,7 @@
 #include "PacketHandler.h"
 #include "AsyncLogger.h"
 #include "DBManager.h"
+#include <cstring>
 
 PacketHandler& PacketHandler::GetInstance() {
     static PacketHandler instance;
@@ -50,19 +51,19 @@ void PacketHandler::Handle(std::shared_ptr<Session> session, std::vector<char>& 
 void PacketHandler::OnAdventureInfo(std::shared_ptr<Session> session, PKT_Adventure* packet) {
     AsyncLogger::GetInstance().Log(
         "ADVENTURE_INFO 수신. adventure_id=" + std::to_string(packet->adventure_id) +
-        " name=" + std::string(packet->adventure_name));
+        " name=" + std::string(packet->adventure_name, strnlen(packet->adventure_name, sizeof(packet->adventure_name))));
 }
 
 void PacketHandler::OnGuildInfo(std::shared_ptr<Session> session, PKT_Guild* packet) {
     AsyncLogger::GetInstance().Log(
         "GUILD_INFO 수신. guild_id=" + std::to_string(packet->guild_id) +
-        " name=" + std::string(packet->guild_name));
+        " name=" + std::string(packet->guild_name, strnlen(packet->guild_name, sizeof(packet->guild_name))));
 }
 
 void PacketHandler::OnCharacterInfo(std::shared_ptr<Session> session, PKT_Character* packet) {
     AsyncLogger::GetInstance().Log(
         "CHARACTER_INFO 수신. character_id=" + std::to_string(packet->character_id) +
-        " nickname=" + std::string(packet->nickname));
+        " nickname=" + std::string(packet->nickname, strnlen(packet->nickname, sizeof(packet->nickname))));
 
     if (!DBManager::GetInstance().InsertCharacter(*packet)) {
         AsyncLogger::GetInstance().LogError(
@@ -84,7 +85,7 @@ void PacketHandler::OnCharacterStat(std::shared_ptr<Session> session, PKT_Charac
 void PacketHandler::OnItemDictionary(std::shared_ptr<Session> session, PKT_ItemDictionary* packet) {
     AsyncLogger::GetInstance().Log(
         "ITEM_DICTIONARY_INFO 수신. item_dict_id=" + std::to_string(packet->item_dict_id) +
-        " name=" + std::string(packet->item_name));
+        " name=" + std::string(packet->item_name, strnlen(packet->item_name, sizeof(packet->item_name))));
 }
 
 void PacketHandler::OnItemInstance(std::shared_ptr<Session> session, PKT_ItemInstance* packet) {
